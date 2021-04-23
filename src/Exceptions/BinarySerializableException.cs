f2ebfaf11a1bec66d72edcc7ef00e962e2a7ca2e
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-04-23 09:38:31 +0200

    Add BinarySerializableException

---
+++ src/Exceptions/BinarySerializableException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BinarySerializer
+{
+    [Serializable]
+    public class BinarySerializableException : Exception
+    {
+        public BinarySerializableException(BinarySerializable data) : base(FormatMessage(data, null)) { }
+
+        public BinarySerializableException(BinarySerializable data, string message) : base(FormatMessage(data, message)) { }
+
+        public BinarySerializableException(BinarySerializable data, string message, Exception inner) : base(FormatMessage(data, message), inner) { }
+
+        protected BinarySerializableException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        protected static string FormatMessage(BinarySerializable data, string message) => $"{data?.Offset}{(message == null || data == null ? message : $": {message}")}";
+    }
+}
