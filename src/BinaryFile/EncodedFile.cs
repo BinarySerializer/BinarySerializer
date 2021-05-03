c94efc5907967a159270b47548e84aa6b8ece61a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-05-03 20:50:43 +0200

    Add EncodedFile

---
+++ src/BinaryFile/EncodedFile.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+
+namespace BinarySerializer
+{
+    public class EncodedFile : BinaryFile
+	{
+        public EncodedFile(Context context, string filePath, IStreamEncoder encoder, Endian endianness = Endian.Little, long fileLength = 0) : base(context, filePath, endianness)
+        {
+            Encoder = encoder;
+            length = fileLength;
+        }
+
+        public IStreamEncoder Encoder { get; }
+
+		private long length;
+        public override long Length
+        {
+            get
+            {
+                if (length == 0)
+                {
+                    // Open the file
+                    using Stream s = FileManager.GetFileReadStream(AbsolutePath);
+
+                    // Decode the file
+                    using var decoded = Encoder.DecodeStream(s);
+
+                    // Set the length
+                    length = decoded.Length;
+                }
+				return length;
+            }
+        }
+
+		public override Reader CreateReader() 
+        {
+			// Open the file
+			using Stream s = FileManager.GetFileReadStream(AbsolutePath);
+
+			// Decode the file
+			var decoded = Encoder.DecodeStream(s);
+
+			// Set the length
+			length = decoded.Length;
+
+			// Return a reader
+			return new Reader(decoded, isLittleEndian: Endianness == Endian.Little);
+		}
+
+		public override Writer CreateWriter() 
+        {
+			Stream memStream = new MemoryStream();
+			memStream.SetLength(Length);
+			Writer writer = new Writer(memStream, isLittleEndian: Endianness == Endian.Little);
+			return writer;
+		}
+
+		public override void EndWrite(Writer writer) 
+        {
+			if (writer != null) 
+            {
+				CreateBackupFile();
+
+                using Stream s = FileManager.GetFileWriteStream(AbsolutePath, RecreateOnWrite);
+                writer.BaseStream.Position = 0;
+                using var encoded = Encoder.EncodeStream(writer.BaseStream);
+                encoded.CopyTo(s);
+            }
+			base.EndWrite(writer);
+		}
+
+		public override Pointer GetPointer(uint serializedValue, Pointer anchor = null) 
+        {
+			uint anchorOffset = anchor?.AbsoluteOffset ?? 0;
+
+			if (serializedValue + anchorOffset >= BaseAddress && serializedValue + anchorOffset <= BaseAddress + Length) 
+				return new Pointer(serializedValue, this, anchor: anchor);
+
+			return null;
+		}
+	}
+}
