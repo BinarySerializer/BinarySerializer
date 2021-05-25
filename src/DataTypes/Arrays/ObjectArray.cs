8a5581c1a7973414a4dd76393f52867e9e015f92
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-05-25 12:35:01 +0200

    Rename ObjectArray.Length to ObjectArray.Pre_Length

--- src/DataTypes/Arrays/ObjectArray.cs
+++ src/DataTypes/Arrays/ObjectArray.cs
@@ -6,11 +6,11 @@
     /// <typeparam name="T">Generic parameter, should be a <see cref="BinarySerializable"/></typeparam>
     public class ObjectArray<T> : BinarySerializable where T : BinarySerializable, new() 
     {
-        public long Length { get; set; } = 0;
+        public long Pre_Length { get; set; } = 0;
         public T[] Value { get; set; }
 
         public override void SerializeImpl(SerializerObject s) {
-			Value = s.SerializeObjectArray<T>(Value, Length, name: "Value");
+			Value = s.SerializeObjectArray<T>(Value, Pre_Length, name: "Value");
 		}
 	}
 }
