5c9a0ad56c8670c00c86cd666a92bd7db819098b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-12-21 22:13:03 +0100

    Remove OverrideLogPath property from serializer log interface

--- src/Abstraction/ISerializerLog.cs
+++ src/Abstraction/ISerializerLog.cs
@@ -5,7 +5,6 @@ namespace BinarySerializer
     public interface ISerializerLog : IDisposable
     {
         bool IsEnabled { get; }
-        string OverrideLogPath { get; set; }
         void Log(object obj);
     }
 }
