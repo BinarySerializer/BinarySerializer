72ce21beac3cd992c76369245b2e9b73f3d55b3a
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-12-31 09:49:24 +0100

    Add MemorySerializerLog for logging to memory

---
+++ src/Context/SerializerLog/MemorySerializerLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace BinarySerializer
+{
+    /// <summary>
+    /// A serializer log which logs to memory, allowing the entire log to be retrieved as a string. This is not recommended
+    /// for larger serializations as it can use a lot of memory.
+    /// </summary>
+    public class MemorySerializerLog : ISerializerLog
+    {
+        public bool IsEnabled => true;
+
+        private StringBuilder _stringBuilder = new StringBuilder();
+        public string GetString => _stringBuilder?.ToString();
+
+        public void Log(object obj) => _stringBuilder?.AppendLine(obj != null ? obj.ToString() : String.Empty);
+        public void Dispose() => _stringBuilder = null;
+    }
+}
