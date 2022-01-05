4d01fcdbeab4bab0347f29b510e1a7adfd2a8068
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-01-05 19:57:14 +0100

    Add extension methods for using an encoder with a byte array

---
+++ src/IO/Encoding/StreamEncoderExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace BinarySerializer
+{
+    /// <summary>
+    /// Extension methods for <see cref="IStreamEncoder"/>
+    /// </summary>
+    public static class StreamEncoderExtensions
+    {
+        /// <summary>
+        /// Decodes data from a byte array
+        /// </summary>
+        /// <param name="encoder">The encoder</param>
+        /// <param name="data">The data to decode</param>
+        /// <returns>The decoded data</returns>
+        public static byte[] DecodeBuffer(this IStreamEncoder encoder, byte[] data)
+        {
+            // Create memory streams
+            using MemoryStream inputStream = new MemoryStream(data);
+            using MemoryStream outputStream = new MemoryStream();
+
+            // Decode the data
+            encoder.DecodeStream(inputStream, outputStream);
+
+            // Return the output
+            return outputStream.ToArray();
+        }
+
+        /// <summary>
+        /// Encodes data from a byte array
+        /// </summary>
+        /// <param name="encoder">The encoder</param>
+        /// <param name="data">The data to encode</param>
+        /// <returns>The encoded data</returns>
+        public static byte[] EncodeBuffer(this IStreamEncoder encoder, byte[] data)
+        {
+            // Create memory streams
+            using MemoryStream inputStream = new MemoryStream(data);
+            using MemoryStream outputStream = new MemoryStream();
+
+            // Encode the data
+            encoder.EncodeStream(inputStream, outputStream);
+
+            // Return the output
+            return outputStream.ToArray();
+        }
+    }
+}
