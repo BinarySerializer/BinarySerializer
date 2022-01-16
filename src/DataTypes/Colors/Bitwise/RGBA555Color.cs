62709a182981b86b08d4297181f758cba0d10947
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-01-16 19:17:10 +0100

    Add RGBA555Color

---
+++ src/DataTypes/Colors/Bitwise/RGBA555Color.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BinarySerializer
+{
+    public class RGBA555Color : BaseBitwiseColor
+    {
+        public RGBA555Color() { }
+        public RGBA555Color(float r, float g, float b, float a = 1f) : base(r, g, b, a) { }
+        public RGBA555Color(uint colorValue) : base(colorValue) { }
+
+        protected override IReadOnlyDictionary<ColorChannel, ColorChannelFormat> ColorFormatting => Format;
+
+        protected static IReadOnlyDictionary<ColorChannel, ColorChannelFormat> Format = new Dictionary<ColorChannel, ColorChannelFormat>()
+        {
+            [ColorChannel.Red] = new ColorChannelFormat(0, 5),
+            [ColorChannel.Green] = new ColorChannelFormat(5, 5),
+            [ColorChannel.Blue] = new ColorChannelFormat(10, 5),
+        };
+    }
+}
