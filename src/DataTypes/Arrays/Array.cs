1cd36fc68a62c9a48a1567a2d7b25c460c4baea0
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-01-17 13:04:44 +0100

    Add implicit operators to array types

--- src/DataTypes/Arrays/Array.cs
+++ src/DataTypes/Arrays/Array.cs
@@ -6,12 +6,15 @@
     /// <typeparam name="T">Generic value parameter, should not be <see cref="BinarySerializable"/></typeparam>
     public class Array<T> : BinarySerializable 
     {
-		public long Length { get; set; } = 0;
-		public T[] Value { get; set; }
+        public long Pre_Length { get; set; } = 0;
+        public T[] Value { get; set; }
 
-		public override void SerializeImpl(SerializerObject s) 
+        public static implicit operator T[](Array<T> array) => array.Value;
+        public static implicit operator Array<T>(T[] array) => new Array<T>() { Pre_Length = array.Length, Value = array };
+
+        public override void SerializeImpl(SerializerObject s) 
         {
-			Value = s.SerializeArray<T>(Value, Length, name: "Value");
-		}
-	}
+            Value = s.SerializeArray<T>(Value, Pre_Length, name: "Value");
+        }
+    }
 }
--- src/DataTypes/Arrays/ObjectArray.cs
+++ src/DataTypes/Arrays/ObjectArray.cs
@@ -4,13 +4,18 @@
     /// Array of serializable objects. Mainly for files that are just simple object arrays. Use <see cref="SerializerObject.SerializeObjectArray"/> when possible
     /// </summary>
     /// <typeparam name="T">Generic parameter, should be a <see cref="BinarySerializable"/></typeparam>
-    public class ObjectArray<T> : BinarySerializable where T : BinarySerializable, new() 
+    public class ObjectArray<T> : BinarySerializable 
+        where T : BinarySerializable, new() 
     {
         public long Pre_Length { get; set; } = 0;
         public T[] Value { get; set; }
 
-        public override void SerializeImpl(SerializerObject s) {
-			Value = s.SerializeObjectArray<T>(Value, Pre_Length, name: "Value");
-		}
-	}
+        public static implicit operator T[](ObjectArray<T> array) => array.Value;
+        public static implicit operator ObjectArray<T>(T[] array) => new ObjectArray<T>() { Pre_Length = array.Length, Value = array };
+
+        public override void SerializeImpl(SerializerObject s) 
+        {
+            Value = s.SerializeObjectArray<T>(Value, Pre_Length, name: "Value");
+        }
+    }
 }
