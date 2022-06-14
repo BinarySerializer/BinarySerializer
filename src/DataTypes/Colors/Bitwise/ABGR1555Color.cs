38d354435ff683fa67a91867657142002f84ce16
byvar <39142447+byvar@users.noreply.github.com>
2022-06-14 18:41:54 +0200

    Add ABGR1555Color

---
+++ src/DataTypes/Colors/Bitwise/ABGR1555Color.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BinarySerializer
+{
+    /// <summary>
+    /// A standard ARGB color wrapper with serializing support for the encoding ABGR-1555
+    /// </summary>
+    public class ABGR1555Color : BaseBitwiseColor
+    {
+        public ABGR1555Color() { }
+        public ABGR1555Color(float r, float g, float b, float a = 1f) : base(r, g, b, a) { }
+        public ABGR1555Color(uint colorValue) : base(colorValue) { }
+
+        protected override IReadOnlyDictionary<ColorChannel, ColorChannelFormat> ColorFormatting => Format;
+
+        protected static IReadOnlyDictionary<ColorChannel, ColorChannelFormat> Format = new Dictionary<ColorChannel, ColorChannelFormat>()
+        {
+            [ColorChannel.Alpha] = new ColorChannelFormat(0, 1),
+            [ColorChannel.Blue] = new ColorChannelFormat(1, 5),
+            [ColorChannel.Green] = new ColorChannelFormat(6, 5),
+            [ColorChannel.Red] = new ColorChannelFormat(11, 5),
+        };
+    }
+}
