5840b61a6d468e9fc04a2151ac93a5622882ac9b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-07-24 16:10:12 +0200

    Add exception for unsupported format versions

---
+++ src/Exceptions/UnsupportedFormatVersionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BinarySerializer
+{
+    [Serializable]
+    public class UnsupportedFormatVersionException : BinarySerializableException
+    {
+        public UnsupportedFormatVersionException(BinarySerializable data) : base(data) { }
+        public UnsupportedFormatVersionException(BinarySerializable data, string message) : base(data, message) { }
+        public UnsupportedFormatVersionException(BinarySerializable data, string message, Exception inner) : base(data, message, inner) { }
+        protected UnsupportedFormatVersionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
