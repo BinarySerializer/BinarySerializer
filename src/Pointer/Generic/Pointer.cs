8116ed03e16fc825dc1315153cf427a3df788de1
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-07-27 22:10:19 +0200

    Enable nullable for Pointer<T>

--- src/Pointer/Generic/Pointer.cs
+++ src/Pointer/Generic/Pointer.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿#nullable enable
+using System;
 
 namespace BinarySerializer
 {
     public class Pointer<T>
     {
-        public Pointer(Pointer pointerValue, T value = default)
+        public Pointer(Pointer? pointerValue, T? value = default)
         {
             Context = pointerValue?.Context;
             PointerValue = pointerValue;
@@ -16,32 +17,37 @@ namespace BinarySerializer
             Value = default;
         }
 
-        public Context Context { get; private set; }
-        public Pointer PointerValue { get; }
-        public T Value { get; set; }
+        public Context? Context { get; private set; }
+        public Pointer? PointerValue { get; }
+        public T? Value { get; set; }
 
-        public Pointer<T> ResolveValue(SerializerObject s, PointerFunctions.SerializeFunction<T> func) {
+        public Pointer<T> ResolveValue(SerializerObject s, PointerFunctions.SerializeFunction<T> func) 
+        {
             if (s == null) 
                 throw new ArgumentNullException(nameof(s));
+            if (func == null) 
+                throw new ArgumentNullException(nameof(func));
 
             Context = s.Context;
 
             if (PointerValue == null) 
                 return this;
 
-            s.DoAt(PointerValue, () => {
-                Value = func(s, Value, name: nameof(Value));
-            });
+            s.DoAt(PointerValue, () => Value = func(s, Value, name: nameof(Value)));
 
             return this;
         }
 
-        public static implicit operator T(Pointer<T> a) => a.Value;
-        public static implicit operator Pointer(Pointer<T> a) => a?.PointerValue;
+        public static implicit operator T?(Pointer<T> a) => a.Value;
+        public static implicit operator Pointer?(Pointer<T>? a) => a?.PointerValue;
 
-        public static Pointer<U> FromObject<U>(U obj) where U : BinarySerializable, new() {
-            if(obj == null) return new Pointer<U>();
-            return new Pointer<U>(obj?.Offset, value: obj);
+        public static Pointer<U> FromObject<U>(U? obj) 
+            where U : BinarySerializable, new() 
+        {
+            if (obj == null) 
+                return new Pointer<U>();
+
+            return new Pointer<U>(obj.Offset, value: obj);
         }
     }
 }
