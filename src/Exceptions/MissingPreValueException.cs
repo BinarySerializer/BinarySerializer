0887346a58fa143307f8a04a593a5e36e8588748
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-08-14 21:55:10 +0200

    Add MissingPreValueException

---
+++ src/Exceptions/MissingPreValueException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BinarySerializer
+{
+    [Serializable]
+    public class MissingPreValueException : BinarySerializableException
+    {
+        public MissingPreValueException(BinarySerializable data, string preValueName) : base(data, $"Missing required pre-value {preValueName}")
+        {
+            PreValueName = preValueName;
+        }
+        public MissingPreValueException(BinarySerializable data, string preValueName, string message) : base(data,
+            message)
+        {
+            PreValueName = preValueName;
+        }
+        public MissingPreValueException(BinarySerializable data, string preValueName, string message, Exception inner) : base(data, message, inner) { }
+        protected MissingPreValueException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public string PreValueName { get; }
+    }
+}
