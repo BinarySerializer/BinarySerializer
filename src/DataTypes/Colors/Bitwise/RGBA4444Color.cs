ae1df73958d175b783a0009096695def8de16d38
byvar <39142447+byvar@users.noreply.github.com>
2022-08-26 22:07:55 +0200

    Add RGBA4444 color

---
+++ src/DataTypes/Colors/Bitwise/RGBA4444Color.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BinarySerializer
+{
+    /// <summary>
+    /// A standard ARGB color wrapper with serializing support for the encoding RGBA-4444
+    /// </summary>
+    public class RGBA4444Color : BaseBitwiseColor
+    {
+        public RGBA4444Color() { }
+        public RGBA4444Color(float r, float g, float b, float a = 1f) : base(r, g, b, a) { }
+        public RGBA4444Color(uint colorValue) : base(colorValue) { }
+
+        protected override IReadOnlyDictionary<ColorChannel, ColorChannelFormat> ColorFormatting => Format;
+
+        protected static IReadOnlyDictionary<ColorChannel, ColorChannelFormat> Format = new Dictionary<ColorChannel, ColorChannelFormat>()
+        {
+            [ColorChannel.Red] = new ColorChannelFormat(0, 4),
+            [ColorChannel.Green] = new ColorChannelFormat(4, 4),
+            [ColorChannel.Blue] = new ColorChannelFormat(8, 4),
+            [ColorChannel.Alpha] = new ColorChannelFormat(12, 4),
+        };
+    }
+}
