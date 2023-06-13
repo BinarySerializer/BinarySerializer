d8918444ba9ccfc1a827ced030bf1b2b609bf6f7
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-06-13 20:17:52 +0200

    Update VersionTree, have node implement IEnumerable

--- src/Context/VersionTree/VersionTree.cs
+++ src/Context/VersionTree/VersionTree.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,36 +20,34 @@ namespace BinarySerializer
         public void Init() => Root.PropagateParents();
 
         public bool HasParent(T version) => Current?.HasParent(version) == true;
+        public bool HasAnyParent(params T[] versions) => versions.Any(HasParent);
         public Node? FindVersion(T version) => Root.FindVersion(version);
 
-        public class Node
+        public class Node : IEnumerable<Node>
         {
             public Node(T version)
             {
                 Version = version;
+                Children = new List<Node>();
                 ParentVersions = new HashSet<T>();
             }
 
-            public T Version { get; }
-            protected HashSet<T> ParentVersions { get; }
+            private List<Node> Children { get; }
+            private HashSet<T> ParentVersions { get; }
 
-            public Node[]? Children { get; set; }
+            public T Version { get; }
 
             public bool HasParent(T version) => Version.Equals(version) || ParentVersions.Contains(version);
-
-            public Node? FindVersion(T version) 
+            public Node? FindVersion(T version)
             {
-                if (Version.Equals(version)) 
+                if (Version.Equals(version))
                     return this;
 
-                return Children?.Select(child => child.FindVersion(version)).FirstOrDefault(result => result != null);
+                return Children.Select(child => child.FindVersion(version)).FirstOrDefault(result => result != null);
             }
 
             public void PropagateParents() 
             {
-                if (Children == null) 
-                    return;
-
                 foreach (Node child in Children) 
                 {
                     foreach (T pv in ParentVersions)
@@ -59,11 +58,11 @@ namespace BinarySerializer
                 }
             }
 
-            public Node SetChildren(params Node[] nodes) 
-            {
-                Children = nodes;
-                return this;
-            }
+            public void Add(Node node) => Children.Add(node);
+            public void AddRange(IEnumerable<Node> nodes) => Children.AddRange(nodes);
+
+            public IEnumerator<Node> GetEnumerator() => Children.GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
     }
 }
