a2f329f719572b8397a720c747ee21e7c3466922
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-05-05 10:38:09 +0200

    Add UserDefinedValueProcessor

---
+++ src/IO/BinaryProcessor/DataLength/UserDefinedValueProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BinarySerializer
+{
+    public class UserDefinedValueProcessor : CalculatedValueProcessor
+    {
+        public UserDefinedValueProcessor(Func<long> getValueFunc)
+        {
+            GetValueFunc = getValueFunc;
+        }
+
+        private Func<long> GetValueFunc { get; }
+
+        public override void EndProcessing(SerializerObject s)
+        {
+            CalculatedValue = GetValueFunc();
+            base.EndProcessing(s);
+        }
+    }
+}
