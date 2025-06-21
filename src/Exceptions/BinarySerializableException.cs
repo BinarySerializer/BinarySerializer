f286eea96df312067480607ac334fa4145e1cdba
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2025-06-21 22:37:17 +0200

    Add new ctors to BinarySerializableException

--- src/Exceptions/BinarySerializableException.cs
+++ src/Exceptions/BinarySerializableException.cs
@@ -7,8 +7,14 @@ namespace BinarySerializer
     [Serializable]
     public class BinarySerializableException : Exception
     {
+        public BinarySerializableException(SerializerObject? serializerObject) 
+            : base(FormatMessage(serializerObject, null)) { }
+
         public BinarySerializableException(BinarySerializable? data) 
             : base(FormatMessage(data, null)) { }
+
+        public BinarySerializableException(SerializerObject? serializerObject, string? message)
+            : base(FormatMessage(serializerObject, null)) { }
 
         public BinarySerializableException(BinarySerializable? data, string? message) 
             : base(FormatMessage(data, message)) { }
@@ -18,6 +24,9 @@ namespace BinarySerializer
 
         protected BinarySerializableException(SerializationInfo info, StreamingContext context) 
             : base(info, context) { }
+
+        protected static string FormatMessage(SerializerObject? serializerObject, string? message) => 
+            $"{serializerObject?.CurrentPointer}{(message == null || serializerObject == null ? message : $": {message}")}";
 
         protected static string FormatMessage(BinarySerializable? data, string? message) => 
             $"{data?.Offset}{(message == null || data == null ? message : $": {message}")}";
