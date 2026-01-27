885269f910f536939b19f071c3d2a8f4d91ce861
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-01-27 21:42:20 +0100

    Make sure directory exists when writing file

--- src/IO/FileManager/DefaultFileManager.cs
+++ src/IO/FileManager/DefaultFileManager.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading.Tasks;
@@ -11,7 +12,14 @@ namespace BinarySerializer
         public virtual bool FileExists([NotNullWhen(true)] string? path) => File.Exists(path);
 
         public virtual Stream GetFileReadStream(string path) => File.OpenRead(path);
-        public virtual Stream GetFileWriteStream(string path, bool recreateOnWrite = true) => recreateOnWrite ? File.Create(path) : File.OpenWrite(path);
+        public virtual Stream GetFileWriteStream(string path, bool recreateOnWrite = true)
+        {
+            string? dir = Path.GetDirectoryName(path);
+            if (!String.IsNullOrEmpty(dir))
+                Directory.CreateDirectory(dir);
+            
+            return recreateOnWrite ? File.Create(path) : File.OpenWrite(path);
+        }
 
         public virtual PathSeparatorChar SeparatorCharacter => PathSeparatorChar.ForwardSlash;
 
