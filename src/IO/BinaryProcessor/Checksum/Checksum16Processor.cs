0e13bc1085b555fab3d75131d5669ad1c2e437d2
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-02-01 14:22:34 +0100

    Allow setting value size for Checksum16Processor

--- src/IO/BinaryProcessor/Checksum/Checksum16Processor.cs
+++ src/IO/BinaryProcessor/Checksum/Checksum16Processor.cs
@@ -2,17 +2,24 @@
 {
     public class Checksum16Processor : ChecksumProcessor
     {
-        public Checksum16Processor(bool invertBits = false)
+        public Checksum16Processor(bool invertBits = false, int valueSize = 1)
         {
             InvertBits = invertBits;
+            ValueSize = valueSize;
         }
 
         private ushort _checksumValue;
+        private int _valueIndex;
 
         /// <summary>
         /// Indicates if the checksum value bits should be inverted
         /// </summary>
         public bool InvertBits { get; }
+
+        /// <summary>
+        /// The size of a value to calculate, in bytes
+        /// </summary>
+        public int ValueSize { get; }
 
         public override long CalculatedValue
         {
@@ -24,7 +31,7 @@
         {
             int end = offset + count;
             for (int i = offset; i < end; i++)
-                _checksumValue = (ushort)(_checksumValue + buffer[i]);
+                _checksumValue = (ushort)(_checksumValue + (buffer[i] << ((_valueIndex++ % ValueSize) * 8)));
         }
     }
 }
