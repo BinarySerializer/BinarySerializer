85d4bfd8c05630baafcf99fc8ff6cf914de85b59
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-02-22 14:28:27 +0100

    Add option to invert bits for Checksum8Processor

--- src/IO/BinaryProcessor/Checksum/Checksum8Processor.cs
+++ src/IO/BinaryProcessor/Checksum/Checksum8Processor.cs
@@ -2,11 +2,29 @@
 {
     public class Checksum8Processor : ChecksumProcessor
     {
+        public Checksum8Processor(bool invertBits = false)
+        {
+            InvertBits = invertBits;
+        }
+
+        private byte _checksumValue;
+
+        /// <summary>
+        /// Indicates if the checksum value bits should be inverted
+        /// </summary>
+        public bool InvertBits { get; }
+
+        public override long CalculatedValue
+        {
+            get => (byte)(InvertBits ? ~_checksumValue : _checksumValue);
+            set => _checksumValue = (byte)(InvertBits ? ~value : value);
+        }
+
         public override void ProcessBytes(byte[] buffer, int offset, int count)
         {
             int end = offset + count;
             for (int i = offset; i < end; i++)
-                CalculatedValue = (CalculatedValue + buffer[i]) % 256;
+                _checksumValue = (byte)(_checksumValue + buffer[i]);
         }
     }
 }
